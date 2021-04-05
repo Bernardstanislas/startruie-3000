f1ca5f0edc2aac2382d3e5b2bfb2ba551408d538
Stanislas Bernard <bernardstanislas@gmail.com>
2021-04-05 14:28:12 +0200

    :sparkles: Add empty slaughter service

---
+++ src/domain/slaughter/services/slaughter.service.test.ts
@@ -0,0 +1,8 @@
+import { SlaughterService } from "./slaughter.service";
+
+describe("The SlaughterService", () => {
+    it("exists", () => {
+        const service = new SlaughterService();
+        expect(service).toBeDefined();
+    });
+});
